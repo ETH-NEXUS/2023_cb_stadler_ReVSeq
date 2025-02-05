77faf7bc491b9aa5900c37e60d048813bc8377c0
anastasia-escher <anastasia.escher@gmail.com>
2025-02-05 11:58:56 +0100

    [UPD] add serotype to the sample payload

--- api/app/core/management/commands/ena_upload.py
+++ api/app/core/management/commands/ena_upload.py
@@ -155,6 +155,7 @@ class Command(BaseCommand):
         now = dt.datetime.now().strftime('%Y%m%d%H%M%S%f')
         sorted_sample_counts = sorted(sample_counts, key=self.__sort_key, reverse=True)
         taxon_id = sorted_sample_counts[0].substrain.taxon_id
+        serotype = sorted_sample_counts[0].serotype
         metadata = Metadata.objects.filter(sample=sample).first()
         collection_date = metadata.ent_date.strftime("%Y-%m-%d")
         geo_location = metadata.prescriber
@@ -170,7 +171,7 @@ class Command(BaseCommand):
                 _files.append(file.path)
                 logger.debug(f'Adding {file.path} to SER for {sample}')
 
-        return {
+        payload = {
             'template': 'default',
             'data': {
                 'sample': {
@@ -200,6 +201,10 @@ class Command(BaseCommand):
             'files': _files
         }
 
+        if serotype:
+            payload['data']['sample']['serotype'] = serotype
+
+        return payload
 
 
     def upload_analysis_job_and_files(self, job_id, sample, files, analysis_payload):
